df504107ba4f5e21564cd363ab4042126129bfb6
laktosterror <55037659+laktosterror@users.noreply.github.com>
2024-10-02 00:20:30 +0200

    implemented rat movement in overridden move method. - Now also checks for collision before moving.

--- ConsoleCrawler/Rat.cs
+++ ConsoleCrawler/Rat.cs
@@ -4,6 +4,21 @@ public class Rat(int posY, int posX) : Enemy(posY, posX)
 {
     public override char ElementType { get; } = 'r';
     public override ConsoleColor ElementColor { get; } = ConsoleColor.DarkGreen;
-
     
+    private Random random = new Random();
+    public override void Move(Player player, List<LevelElement> elements) {
+        int direction = random.Next(4); // 0: up, 1: down, 2: left, 3: right
+        int newX = this.PosX, newY = this.PosY;
+        
+        if (direction == 0) newY++;
+        else if (direction == 1) newY--;
+        else if (direction == 2) newX--;
+        else if (direction == 3) newX++;
+        
+        // Check for collision before moving
+        if (!CheckCollision(newX, newY, elements)) {
+            this.PosX = newX;
+            this.PosY = newY;
+        }
+    }
 }
