76a4c3f2edd6762a9cd476bf98b192c8f1af7742
laktosterror <55037659+laktosterror@users.noreply.github.com>
2024-10-02 00:19:42 +0200

    implemented snake movement in overridden move method. - Now also checks for collision before moving.

--- ConsoleCrawler/Snake.cs
+++ ConsoleCrawler/Snake.cs
@@ -4,4 +4,21 @@ public class Snake(int posY, int posX) : Enemy(posY, posX)
 {
     public override char ElementType { get; } = 's';
     public override ConsoleColor ElementColor { get; } = ConsoleColor.Magenta;
+    
+    public override void Move(Player player, List<LevelElement> elements) {
+        if (Math.Abs(this.PosX - player.PosX) <= 1 && Math.Abs(this.PosY - player.PosY) <= 1) {
+            int newX = this.PosX, newY = this.PosY;
+            
+            if (this.PosX < player.PosX) newX--;
+            else if (this.PosX > player.PosX) newX++;
+            else if (this.PosY < player.PosY) newY--;
+            else if (this.PosY > player.PosY) newY++;
+            
+            // Check for collision before moving
+            if (!CheckCollision(newX, newY, elements)) {
+                this.PosX = newX;
+                this.PosY = newY;
+            }
+        }
+    }
 }
