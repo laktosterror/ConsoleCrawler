91e8bb43d39a0526541540a21df6828b2f1724ba
laktosterror <55037659+laktosterror@users.noreply.github.com>
2024-10-02 00:18:26 +0200

    method for checking collision is now public - changed class name, plural to singular

--- ConsoleCrawler/AliveElement.cs
+++ ConsoleCrawler/AliveElement.cs
@@ -1,10 +1,10 @@
 ﻿namespace ConsoleCrawler;
 
-public abstract class AliveElements(int posY, int posX) : LevelElement(posY, posX)
+public abstract class AliveElement(int posY, int posX) : LevelElement(posY, posX)
 {
     public abstract void Move(Player player, List<LevelElement> elements);
     
-    bool CheckCollision(int newX, int newY, List<LevelElement> elements) {
+    public bool CheckCollision(int newX, int newY, List<LevelElement> elements) {
         foreach (LevelElement element in elements) {
             if (this != element && newX == element.PosX && newY == element.PosY) {
                 return true;
@@ -12,5 +12,4 @@ public abstract class AliveElements(int posY, int posX) : LevelElement(posY, pos
         }
         return false;
     }
-    
 }
